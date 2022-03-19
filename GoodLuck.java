e17cf226bb31f697a5c090ab21b896fe8f15fb9e
triyaa03 <92058853+triyaa03@users.noreply.github.com>
2022-03-19 02:20:21 -0400

    Add files via upload

---
+++ GoodLuck.java
@@ -0,0 +1,14 @@
+package com.example.whatnumberamithinkingof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GoodLuck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_good_luck);
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.whatnumberamithinkingof;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button =  findViewById(R.id.normal);
+
+    }
+}
