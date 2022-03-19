aec7eaaa16df97a496fcaf9d9070a4e49c993245
triyaa03 <92058853+triyaa03@users.noreply.github.com>
2022-03-19 02:49:57 -0400

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -2,11 +2,14 @@ package com.example.whatnumberamithinkingof;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    //private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +17,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button =  findViewById(R.id.normal);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNormal();
+            }
+        });
 
+        button =  findViewById(R.id.hard);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHard();
+            }
+        });
+
+        button =  findViewById(R.id.goodluck);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGoodLuck();
+            }
+        });
+
+        button =  findViewById(R.id.scores);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openScores();
+            }
+        });
+
+        }
+
+        public void openNormal() {
+            Intent intent = new Intent(this, Normal.class);
+            startActivity(intent);
+
+        }
+
+        public void openHard() {
+            Intent intent = new Intent(this, Hard.class);
+            startActivity(intent);
+
+        }
+
+        public void openGoodLuck() {
+            Intent intent = new Intent(this, GoodLuck.class);
+            startActivity(intent);
+
+        }
+
+        public void openScores() {
+            Intent intent = new Intent(this, Scores.class);
+            startActivity(intent);
+
+        }
     }
-}
