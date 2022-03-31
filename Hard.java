59afe00d4a6327e4030020c2ed79209239d9d25f
triyaa03 <92058853+triyaa03@users.noreply.github.com>
2022-03-31 13:26:55 -0400

    Update Hard.java

--- Hard.java
+++ Hard.java
@@ -3,6 +3,9 @@ package com.example.whatnumberamithinkingof;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Hard extends AppCompatActivity {
 
@@ -11,4 +14,16 @@ public class Hard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hard);
     }
+
+    public void HardClicked(View v) {
+        EditText input = (EditText) findViewById(R.id.inputBox2);
+        String inputA = input.getText().toString();
+
+        double i = HardModel.toDoubleHard(inputA);
+        double answerA = HardModel.answerHard(i);
+        String responseA = HardModel.formatAnswerHard(answerA);
+        ((TextView) findViewById(R.id.answer2)).setText(responseA);
+    }
+
+
 }
