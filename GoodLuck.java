58803d1c7cd5aa743ae348d6e120fef3365e90a7
triyaa03 <92058853+triyaa03@users.noreply.github.com>
2022-03-31 13:27:31 -0400

    Update GoodLuck.java

--- GoodLuck.java
+++ GoodLuck.java
@@ -3,6 +3,9 @@ package com.example.whatnumberamithinkingof;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GoodLuck extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class GoodLuck extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_good_luck);
+    }
+
+    public void GoodLuckClicked(View v) {
+        EditText input = (EditText) findViewById(R.id.inputBox3);
+        String inputA = input.getText().toString();
+
+        double i = GoodLuckModel.toDoubleGoodLuck(inputA);
+        double answerA = GoodLuckModel.answerGoodLuck(i);
+        String responseA = GoodLuckModel.formatAnswerGoodLuck(answerA);
+        ((TextView) findViewById(R.id.answer3)).setText(responseA);
     }
 }
