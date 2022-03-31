1833425a1f753508b6793e05a866a7e5daee0a19
triyaa03 <92058853+triyaa03@users.noreply.github.com>
2022-03-31 13:26:17 -0400

    Update Normal.java

--- Normal.java
+++ Normal.java
@@ -3,6 +3,9 @@ package com.example.whatnumberamithinkingof;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Normal extends AppCompatActivity {
 
@@ -11,4 +14,17 @@ public class Normal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_normal);
     }
+
+    public void NormalClicked(View v) {
+        EditText input = (EditText) findViewById(R.id.inputBox);
+        String inputA = input.getText().toString();
+
+        double i = NormalModel.toDoubleNormal(inputA);
+        double answerA = NormalModel.answerNormal(i);
+        String responseA = NormalModel.formatAnswerNormal(answerA);
+        ((TextView) findViewById(R.id.answer)).setText(responseA);
+    }
+
+
+
 }
